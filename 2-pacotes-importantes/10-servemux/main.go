9a545621470779c26b4ca29b718d2bbb99e41edb
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-12-27 18:31:42 -0300

    :sparkles: Criando servmux

--- 2-pacotes-importantes/10-servemux/main.go
+++ 2-pacotes-importantes/10-servemux/main.go
@@ -1,5 +1,23 @@
 package main
 
+import "net/http"
+
 func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HomeHandler)
+	mux.Handle("/blog", blog{title: "Blog"})
+	http.ListenAndServe(":8080", mux)
+}
+
+func HomeHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte("Hello World!"))
+}
+
+type blog struct {
+	title string
+}
+
+func (b blog) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+	writer.Write([]byte(b.title))
 
 }
