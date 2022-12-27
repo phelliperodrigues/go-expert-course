fe2f9cdce447182edf8c1a2477a43dec14a1da5d
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-12-27 18:39:48 -0300

    :sparkles: Criando serverfile

--- 2-pacotes-importantes/11-file-server/main.go
+++ 2-pacotes-importantes/11-file-server/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./public"))
+	mux := http.NewServeMux()
+	mux.Handle("/", fileServer)
+	mux.HandleFunc("/blog", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("Blog"))
+	})
+	log.Fatal(http.ListenAndServe(":8080", mux))
 
 }
