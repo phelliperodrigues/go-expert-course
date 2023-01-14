78c0138cd1c6759a3bdbb8d2b5657e0dbf2d3760
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 15:47:17 -0300

    Template com webserver

--- 2-pacotes-importantes/15-template-com-webserver/main.go
+++ 2-pacotes-importantes/15-template-com-webserver/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"net/http"
+)
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		t := template.Must(template.New("template.html").
+			ParseFiles("template.html"))
+		err := t.Execute(writer, Cursos{
+			{"Go", 40},
+			{"Java", 50},
+			{"Ruby", 30},
+		})
+		if err != nil {
+			panic(err)
+		}
+	})
+	http.ListenAndServe(":8082", nil)
 }
