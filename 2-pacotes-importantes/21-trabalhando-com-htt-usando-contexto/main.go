97cd3782081fd2731e2fa15ebcfa7d75b6d340fd
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 16:43:15 -0300

    Trabalhando com chamadas http com contexto

--- 2-pacotes-importantes/21-trabalhando-com-htt-usando-contexto/main.go
+++ 2-pacotes-importantes/21-trabalhando-com-htt-usando-contexto/main.go
@@ -1,5 +1,35 @@
 package main
 
-func main() {
+import (
+	"context"
+	"io"
+	"net/http"
+	"time"
+)
 
+func main() {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Microsecond)
+	defer cancel()
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://dummyjson.com/products", nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
 }
