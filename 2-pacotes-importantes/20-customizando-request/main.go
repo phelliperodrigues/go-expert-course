5aef08a0de8c8ff7a5d72b8a96134b7a5ea35458
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 16:34:12 -0300

    Customizando a request

--- 2-pacotes-importantes/20-customizando-request/main.go
+++ 2-pacotes-importantes/20-customizando-request/main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
 func main() {
+	c := http.Client{}
+	req, err := http.NewRequest("GET", "https://dummyjson.com/products", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Header.Set("Accept", "application/json")
+
+	resp, err := c.Do(req)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
 
 }
