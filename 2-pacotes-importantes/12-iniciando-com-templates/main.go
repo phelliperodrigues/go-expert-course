4c56a41c044ab1494332dee0ea151f77d9b9fd1e
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 15:29:54 -0300

    Iniciando com tempaltes

--- 2-pacotes-importantes/12-iniciando-com-templates/main.go
+++ 2-pacotes-importantes/12-iniciando-com-templates/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"os"
+)
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{"Go", 40}
+	tmp := template.New("CursoTemplate")
+	tmp, _ = tmp.Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
+	err := tmp.Execute(os.Stdout, curso)
+	if err != nil {
+		panic(err)
+	}
 }
