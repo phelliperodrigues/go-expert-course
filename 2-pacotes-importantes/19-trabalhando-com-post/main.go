2ac9842dbb90b1200f148f7b3f08cb27702fd911
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 16:29:32 -0300

    Trabalhando com POST method

--- 2-pacotes-importantes/19-trabalhando-com-post/main.go
+++ 2-pacotes-importantes/19-trabalhando-com-post/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"os"
+)
+
 func main() {
+	c := http.Client{}
+	jsonVar := bytes.NewBuffer([]byte(`{"title": "phellipe"}`))
+	resp, err := c.Post("https://dummyjson.com/products/add", "application/json", jsonVar)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+	io.CopyBuffer(os.Stdout, resp.Body, nil)
 
 }
