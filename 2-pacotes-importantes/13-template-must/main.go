84c395f422a84e07fc2afe522907f4a65ff4ab00
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 15:32:07 -0300

    Template Must

--- 2-pacotes-importantes/13-template-must/main.go
+++ 2-pacotes-importantes/13-template-must/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"os"
+)
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+func main() {
+	curso := Curso{"Go", 40}
+	t := template.Must(template.New("CursoTemplate").
+		Parse("Curso: {{.Nome}} - Carga Horária: {{.CargaHoraria}}"))
+	err := t.Execute(os.Stdout, curso)
+	if err != nil {
+		panic(err)
+	}
 }
