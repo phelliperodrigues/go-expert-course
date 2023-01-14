dadef6f6b1005dd91d27b4cd22d4b5a9d7598d0e
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 16:21:21 -0300

    Trabalhando com http client e timeout

--- 2-pacotes-importantes/18-httpclient-com-timeout/main.go
+++ 2-pacotes-importantes/18-httpclient-com-timeout/main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"io"
+	"net/http"
+	"time"
+)
 
+func main() {
+	c := http.Client{Timeout: time.Microsecond}
+	resp, err := c.Get("https://google.com")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	println(string(body))
 }
