ab61c84c17227bf57505960242ab0d503549325f
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-14 15:42:16 -0300

    Template Com Arquivo externo

--- 2-pacotes-importantes/14-template-com-arquivo-externo/main.go
+++ 2-pacotes-importantes/14-template-com-arquivo-externo/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"html/template"
+	"os"
+)
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
+func main() {
+	t := template.Must(template.New("template.html").
+		ParseFiles("template.html"))
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go", 40},
+		{"Java", 50},
+		{"Ruby", 30},
+	})
+	if err != nil {
+		panic(err)
+	}
 }
