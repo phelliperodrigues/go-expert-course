cf94d246f2e3701615f03e1eab9213c01eeeb537
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-25 15:06:38 -0300

    Inserindo dados no db

--- 4-banco-de-dados/go.mod
+++ 4-banco-de-dados/go.mod
@@ -2,4 +2,10 @@ module githum.com/phelliperodrigues/goexpert/database
 
 go 1.19
 
-require github.com/google/uuid v1.3.0
+
+require (
+	github.com/google/uuid v1.3.0
+ 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+)
+
+
--- 4-banco-de-dados/main.go
+++ 4-banco-de-dados/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/google/uuid"
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
 
 type Product struct {
 	ID    string
@@ -16,6 +20,32 @@ func NewProduct(name string, price float64) *Product {
 	}
 }
 
-func main() {
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("insert into products(id, name, price) values (?, ?, ?)")
+	if err != nil {
+		panic(err)
+	}
+	defer stmt.Close()
 
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		panic(err)
+	}
+	return err
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	product := NewProduct("Notebook", 1899.0)
+	err = insertProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
 }
