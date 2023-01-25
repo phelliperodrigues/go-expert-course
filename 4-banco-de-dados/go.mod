f1695013b35747a1537ce222bbce0581b06ae7c3
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-25 14:45:42 -0300

    Iniciando projeto com banco de dados

---
+++ 4-banco-de-dados/go.mod
@@ -0,0 +1,5 @@
+module githum.com/phelliperodrigues/goexpert/database
+
+go 1.19
+
+require github.com/google/uuid v1.3.0
---
+++ 4-banco-de-dados/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "github.com/google/uuid"
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+
+}
