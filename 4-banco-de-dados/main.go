1b46bddd45ce8956b5e98f63f0e14f3fc7e2e3e5
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2023-01-25 16:37:25 -0300

    Selecionando todos registros de produtos

--- 4-banco-de-dados/main.go
+++ 4-banco-de-dados/main.go
@@ -24,13 +24,13 @@ func NewProduct(name string, price float64) *Product {
 func insertProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("insert into products(id, name, price) values (?, ?, ?)")
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer stmt.Close()
 
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	return err
 }
@@ -38,14 +38,14 @@ func insertProduct(db *sql.DB, product *Product) error {
 func updateProduct(db *sql.DB, product *Product) error {
 	stmt, err := db.Prepare("update products set name = ? , price = ? where id = ?")
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	defer stmt.Close()
 
 	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	return err
 }
@@ -53,7 +53,7 @@ func updateProduct(db *sql.DB, product *Product) error {
 func selectProduct(db *sql.DB, id string) (*Product, error) {
 	stmt, err := db.Prepare("select id, name, price from products where id = ? ")
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	defer stmt.Close()
@@ -62,9 +62,30 @@ func selectProduct(db *sql.DB, id string) (*Product, error) {
 
 	err = stmt.QueryRow(id).Scan(&p.ID, &p.Name, &p.Price)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	return &p, nil
+}
+
+func selectProducts(db *sql.DB) ([]Product, error) {
+
+	rows, err := db.Query("select id, name, price  from products")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var products []Product
+
+	for rows.Next() {
+		var p Product
+		err = rows.Scan(&p.ID, &p.Name, &p.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, p)
+	}
+	return products, err
 }
 
 func main() {
@@ -95,5 +116,11 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Printf("%#v", p)
+	fmt.Printf("%#v\n", p)
+
+	products, err := selectProducts(db)
+
+	for _, p := range products {
+		fmt.Printf("%#v\n", p)
+	}
 }
